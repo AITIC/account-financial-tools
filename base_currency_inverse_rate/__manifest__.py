34adb87f81976f972b6948be03579cb19b4a2e06
Katia Logas <klogas@aitic.com.ar>
2021-02-26 12:54:49 -0300

    Migrar 14 el Módulo Multipagos

--- base_currency_inverse_rate/__manifest__.py
+++ base_currency_inverse_rate/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Base Currency Inverse Rate',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'category': 'Accounting',
     'sequence': 14,
     'author': 'ADHOC SA',
