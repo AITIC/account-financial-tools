ae3159ef1ee8cba5df63953773ba114302622201
Katia Logas <klogas@aitic.com.ar>
2021-06-24 09:28:11 -0300

    Cambios para actualizar repositorios

--- account_ux/__manifest__.py
+++ account_ux/__manifest__.py
@@ -59,6 +59,6 @@
     # lo hacemos auto install porque este repo no lo podemos agregar en otros
     # por build de travis (ej sipreco) y queremos que para runbot se auto
     # instale
-    'auto_install': False,
+    'auto_install': True,
     'application': False,
 }
